3062fcb1a5c021cdfa0b043ce08648e534b00498
Unknown <1146923246@qq.com>
2018-04-06 16:05:40 +0800

    merge

--- src/main/java/com/tongyuan/testmp1/service/impl/TraineeServiceImpl.java
+++ src/main/java/com/tongyuan/testmp1/service/impl/TraineeServiceImpl.java
@@ -1,7 +1,14 @@
 package com.tongyuan.testmp1.service.impl;
 
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.tongyuan.testmp1.dao.TraineeMapper;
+import com.tongyuan.testmp1.entity.Trainee;
+import com.tongyuan.testmp1.service.TraineeService;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by zhangcy on 2018/4/6
  */
-public class TraineeServiceImpl {
+@Service
+public class TraineeServiceImpl extends ServiceImpl<TraineeMapper,Trainee> implements TraineeService {
 }
