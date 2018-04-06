607debe40e764b746c6b76d599e15653a01668ac
Unknown <1146923246@qq.com>
2018-04-06 10:59:10 +0800

    测试上传功能

---
+++ src/main/java/com/tongyuan/testmp1/controller/AdminController.java
@@ -0,0 +1,40 @@
+package com.tongyuan.testmp1.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by zhangcy on 2018/4/3
+ */
+@RequestMapping("/admin")
+@Controller
+public class AdminController extends BaseController{
+    @Value("${upload_folder}")
+    private String UPLOADED_FOLDER;
+
+    @GetMapping("/test")
+    public String test(){
+        return "administrator";
+    }
+
+    @PostMapping("/upload")
+    public void upload(@RequestParam("file") MultipartFile file){
+        try{
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+        }catch (IOException e){
+            throw new RuntimeException("上传文件失败");
+        }
+    }
+}
