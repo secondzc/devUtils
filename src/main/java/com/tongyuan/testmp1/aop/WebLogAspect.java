eab1199d28cadca4c0fc93a13da24b04ced4791e
Unknown <1146923246@qq.com>
2018-05-09 10:10:45 +0800

    处理ip

--- src/main/java/com/tongyuan/testmp1/aop/WebLogAspect.java
+++ src/main/java/com/tongyuan/testmp1/aop/WebLogAspect.java
@@ -31,7 +31,8 @@ public class WebLogAspect {
     public void deBefore(JoinPoint joinPoint) throws Throwable {
         ServletRequestAttributes attributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        logger.info("IP : " + request.getRemoteAddr());
+
+        logger.info("IP : " + getIpAddr(request));
         logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
         logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
     }
@@ -43,5 +44,22 @@ public class WebLogAspect {
         HttpServletRequest request = attributes.getRequest();
         logger.error("IP : " + request.getRemoteAddr());
         logger.error("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName()+" 方法执行异常!");
+    }
+
+    public  String getIpAddr(HttpServletRequest request)  {
+        String ip = request.getRemoteAddr();
+        if(ip  ==   null   ||  ip.length()  ==   0   ||   "unknown" .equalsIgnoreCase(ip)){
+            ip  =  request.getHeader( " x-forwarded-for " );
+        }
+        if (ip  ==   null   ||  ip.length()  ==   0   ||   "unknown" .equalsIgnoreCase(ip))  {
+            ip  =  request.getHeader( " Proxy-Client-IP " );
+        }
+        if (ip  ==   null   ||  ip.length()  ==   0   ||   "unknown" .equalsIgnoreCase(ip))  {
+            ip  =  request.getHeader( " WL-Proxy-Client-IP " );
+        }
+        if (ip  ==   null   ||  ip.length()  ==   0   ||   "unknown" .equalsIgnoreCase(ip))  {
+            ip  =  request.getRemoteAddr();
+        }
+        return  ip;
     }
 }
